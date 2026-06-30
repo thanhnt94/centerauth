3a44adf1023fcc8e8882b3397968109e67961052
Thanh Nguyen <thanhnt94@gmail.com>
2026-06-30 23:39:33 +0700

    Auto commit from remote update tool - 2026-06-30 23:39:33

--- sdk/sso_module/routes.py
+++ sdk/sso_module/routes.py
@@ -71,5 +71,8 @@ async def sso_callback(request: Request, response: Response, code: Optional[str]
     await db.commit()
     
     res = RedirectResponse(url="/", status_code=303)
-    res.set_cookie(key="user_id", value=str(user.id), httponly=True)
+    from .cookie_signer import sign_cookie
+    # Fallback to client_secret as key for HMAC signing in SDK template
+    signed_id = sign_cookie(str(user.id), config.client_secret)
+    res.set_cookie(key="user_id", value=signed_id, httponly=True, path="/", samesite="lax", max_age=1800)
     return res

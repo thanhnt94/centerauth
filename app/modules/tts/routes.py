b249be2a364a12b1c4689e75ecd549bced5d6f11
Thanh Nguyen <thanhnt94@gmail.com>
2026-07-04 19:21:14 +0700

    Auto commit from remote update tool - 2026-07-04 19:21:13

--- app/modules/tts/routes.py
+++ app/modules/tts/routes.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter, HTTPException, Depends
 from pydantic import BaseModel
-from typing import Optional, Dict
+from typing import Optional, Dict, Any
 import os
 import json
 import time
